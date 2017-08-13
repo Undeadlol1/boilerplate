c7f83ca0b64e7a89d1ec06e48d0b6750f43c7a7c
Mihail Paley <undeadlol1@mail.ru>
2017-08-13 13:47:35 +0500

    (theme) add darkTheme configs

--- src/browser/theme.js
+++ src/browser/theme.js
@@ -2,33 +2,34 @@
  * 👀 see: http://www.material-ui.com/#/customization/themes 👀
  */
 import {
-  cyan700,
-  grey600,
-  pinkA100, pinkA200, pinkA400,
-  fullWhite,
-} from 'material-ui/styles/colors';
-import {fade} from 'material-ui/utils/colorManipulator'
-import spacing from 'material-ui/styles/spacing'
+  cyan500, cyan700,
+  pinkA200,
+  grey100, grey300, grey400, grey500,
+  white, darkBlack, fullBlack,
+} from 'material-ui/colors';
+import {fade} from 'material-ui/utils/colorManipulator';
+import spacing from 'material-ui/spacing';
 
 const styles = {
   spacing: spacing,
   fontFamily: 'Roboto, sans-serif',
   borderRadius: 2,
   palette: {
-    primary1Color: cyan700,
+    primary1Color: cyan500,
     primary2Color: cyan700,
-    primary3Color: grey600,
+    primary3Color: grey400,
     accent1Color: pinkA200,
-    accent2Color: pinkA400,
-    accent3Color: pinkA100,
-    textColor: fullWhite,
-    secondaryTextColor: fade(fullWhite, 0.7),
-    alternateTextColor: '#303030',
-    canvasColor: '#303030',
-    borderColor: fade(fullWhite, 0.3),
-    disabledColor: fade(fullWhite, 0.3),
-    pickerHeaderColor: fade(fullWhite, 0.12),
-    clockCircleColor: fade(fullWhite, 0.12),
+    accent2Color: grey100,
+    accent3Color: grey500,
+    textColor: darkBlack,
+    secondaryTextColor: fade(darkBlack, 0.54),
+    alternateTextColor: white,
+    canvasColor: white,
+    borderColor: grey300,
+    disabledColor: fade(darkBlack, 0.3),
+    pickerHeaderColor: cyan500,
+    clockCircleColor: fade(darkBlack, 0.07),
+    shadowColor: fullBlack,
   },
 };
 
