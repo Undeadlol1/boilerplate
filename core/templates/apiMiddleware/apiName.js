97705794c36b7bd60e764c7bf87f6b75222e53e1
Mihail Paley <undeadlol1@mail.ru>
2017-08-16 00:40:20 +0500

    (cli) improve api template

--- core/templates/apiMiddleware/apiName.js
+++ core/templates/apiMiddleware/apiName.js
@@ -9,6 +9,50 @@ const limit = 12
 
 router
 
+  // get all moods for index page
+  .get('/:page?', async (req, res) => { // TODO make sure pagination works right
+    try {
+      const page = req.params.page
+      const offset = page ? limit * (page -1) : 0
+      const totalMoods = await Mood.count()
+      const totalPages = Math.ceil(totalMoods / limit)
+      const moods = await Mood.findAll({
+        limit,
+        offset,
+        order: 'rand()',
+        // add preview image
+        include: [{
+          limit: 1,
+          model: Node,
+          order: 'rand()',
+        }]
+      })
+      res.json({ moods, totalPages })
+    }
+    catch (error) {
+      console.log(error);
+      res.boom.internal(error)
+    }
+  })
+
+  // get single mood by slug or name
+  .get('/mood/:slug?', async ({params, query}, res) => {
+    try {
+      const slug = params.slug
+      const name = query.name
+      const mood = await Mood.findOne({
+        where: {
+          $or: [{slug}, {name}]
+        }
+      })
+      res.json(mood)
+    } catch (error) {
+      console.log(error)
+      res.boom.internal(error)
+    }
+  })
+
+  // search for mood
   .get('/search/:name/:page?', async (req, res) => { // TODO make sure pagination works right
     try {
       const { page, name } = req.params
@@ -34,8 +78,9 @@ router
   })
 
   // create mood
-  .post('/', mustLogin, async ({user: { id: UserId }, body: { name }}, res) => {
+  .post('/', mustLogin, async ({user, body: { name }}, res) => {
     try {
+      const UserId = user.id
       const slug = slugify(name)
       const mood = await Mood.create({ UserId, name, slug }) // TODO move this in model definition?
       res.json(mood)
@@ -45,45 +90,15 @@ router
     }
   })
 
-  // get single mood by slug or name
-  .get('/mood/:slug?', async ({params, query}, res) => {
+  // create mood
+  .post('/', mustLogin, async ({user, body: { name }}, res) => {
     try {
-      const slug = params.slug
-      const name = query.name
-      const mood = await Mood.findOne({
-        where: {
-          $or: [{slug}, {name}]
-        }
-      })
+      const UserId = user.id
+      const slug = slugify(name)
+      const mood = await Mood.create({ UserId, name, slug }) // TODO move this in model definition?
       res.json(mood)
     } catch (error) {
       console.log(error)
-      res.boom.internal(error)
-    }
-  })
-
-  // get all moods for index page
-  .get('/:page?', async (req, res) => { // TODO make sure pagination works right
-    try {
-      const page = req.params.page
-      const offset = page ? limit * (page -1) : 0
-      const totalMoods = await Mood.count()
-      const totalPages = Math.ceil(totalMoods / limit)
-      const moods = await Mood.findAll({
-        limit,
-        offset,
-        order: 'rand()',
-        // add preview image
-        include: [{
-          limit: 1,
-          model: Node,
-          order: 'rand()',
-        }]
-      })
-      res.json({ moods, totalPages })
-    }
-    catch (error) {
-      console.log(error);
       res.boom.internal(error)
     }
   })
