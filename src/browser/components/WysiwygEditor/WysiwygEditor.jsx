78599037d10100b59fa7ef0e39f9fea2a2b1ad72
Mihail Paley <undeadlol1@mail.ru>
2017-08-19 00:05:23 +0500

    (WysiwygEditor) fix component rendering

--- src/browser/components/WysiwygEditor/WysiwygEditor.jsx
+++ src/browser/components/WysiwygEditor/WysiwygEditor.jsx
@@ -2,54 +2,46 @@ import cls from 'classnames'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
+// https://jpuri.github.io/react-draft-wysiwyg/#/docs
 import { Editor } from 'react-draft-wysiwyg'
-import { EditorState, ContentState, convertFromHTML } from 'draft-js'
+import { detectLocale } from 'browser/containers/Translator'
+import { EditorState, ContentState, convertFromHTML, } from 'draft-js'
 
 class WysiwygEditor extends Component {
-	state = {
-		editor: null
-	}
-
-	componentDidMount = () => {
-		this.setState({
-			tabs: this.renderEditor()
-		})
-	}
-
-	renderTabs = () => {
-			let defaultState
-			if (process.env.BROWSER) {
-				const blocksFromHTML = convertFromHTML(defaultTexts[index])
-				defaultState = process.env.BROWSER ?
-								EditorState.createWithContent(ContentState.createFromBlockArray(
-									blocksFromHTML.contentBlocks,
-									blocksFromHTML.entityMap
-								))
-							: undefined
-			}
-			return <Editor
-						defaultEditorState={defaultState}
-						onEditorStateChange={this.props.onChange}
-					/>
-	}
-
 	render() {
-		const {props, state} = this
-		const {...rest} = props
-		const className = cls(props.className, "WysiwygEditor")
-		if (!state.editor) return <Editor className={className} {...rest} />
-		else return state.editor
+		const 	{props, state} = this,
+				{...rest} = props,
+				locale = detectLocale(),
+				className = cls(props.className, "WysiwygEditor")
+		let defaultEditorState = EditorState.createEmpty()
+		if (process.env.BROWSER) {
+			const blocksFromHTML = convertFromHTML(props.defaultState)
+			defaultEditorState = EditorState.createWithContent(
+				ContentState.createFromBlockArray(
+					blocksFromHTML.contentBlocks,
+					blocksFromHTML.entityMap
+				)
+			)
+		}
+		return <Editor
+					className={className}
+					defaultEditorState={defaultEditorState}
+					onContentStateChange={this.props.onChange}
+					localization={{locale}}
+					{...rest}
+				/>
 	}
 }
 
 WysiwygEditor.PropTypes = {
-	onChange: PropTypes.func
+	onChange: PropTypes.func,
+	defaultState: PropTypes.object,
 }
 
 export { WysiwygEditor }
 export default connect(
 	// stateToProps
-	(state, ownProps) => ({ ...ownProps }),
+	({user}, ownProps) => ({...ownProps}),
 	// dispatchToProps
     (dispatch, ownProps) => ({})
 )(WysiwygEditor)
