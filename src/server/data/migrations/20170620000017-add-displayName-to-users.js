d1065fc3c78b7276ba8733895aa1553acf41a39d
Mihail Paley <undeadlol1@mail.ru>
2017-09-16 13:51:20 +0500

    (db) fix initial db migration

--- src/server/data/migrations/20170620000017-add-displayName-to-users.js
+++ src/server/data/migrations/20170620000017-add-displayName-to-users.js
@@ -2,14 +2,10 @@
 
 module.exports = {
   up: function (queryInterface, Sequelize) {
-    // doing nothing intentionally
-    return
-    // return  queryInterface.addColumn('users', 'displayName', Sequelize.STRING)
+    return  queryInterface.addColumn('users', 'displayName', Sequelize.STRING)
   },
 
   down: function (queryInterface, Sequelize) {
-    // doing nothing intentionally
-    return
-    // return  queryInterface.removeColumn('users', 'displayName')
+    return  queryInterface.removeColumn('users', 'displayName')
   }
 };
