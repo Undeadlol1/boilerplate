9e3b5723e405a739df97e8c67b855e2a56ccba19
Mihail Paley <undeadlol1@mail.ru>
2017-09-21 19:30:34 +0500

    Improve component template

--- core/templates/component/ComponentName.jsx
+++ core/templates/component/ComponentName.jsx
@@ -2,13 +2,18 @@ import cls from 'classnames'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
+import { Row, Col } from 'react-styled-flexboxgrid'
 import { translate as t } from 'browser/containers/Translator'
 
 class ComponentName extends Component {
 	render() {
 		const {props} = this
 		const className = cls(props.className, "ComponentName")
-		return 	<div className={className}></div>
+		return 	<Row className={className}>
+					<Col xs={12}>
+
+					</Col>
+				</Row>
 	}
 }
 
