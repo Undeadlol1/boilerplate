f504dcc11cd7dbdca3077c2112dd3437863abb8a
Mihail Paley <undeadlol1@mail.ru>
2018-01-08 06:46:04 +0500

    (CommentsList) rework functionality

--- src/browser/components/CommentsList/CommentsList.jsx
+++ src/browser/components/CommentsList/CommentsList.jsx
@@ -2,22 +2,31 @@ import cls from 'classnames'
 import PropTypes from 'prop-types'
 import Paper from 'material-ui/Paper'
 import { connect } from 'react-redux'
-import { VK, Comments } from 'react-vk'
 import React, { Component } from 'react'
 import { Row, Col } from 'react-styled-flexboxgrid'
 import { translate as t } from 'browser/containers/Translator'
 
 class CommentsList extends Component {
+	componentDidMount() {
+		if (process.env.BROWSER) {
+			VK.init({
+				onlyWidgets: true,
+				apiId: process.env.VK_ID,
+			})
+			VK.Widgets.Comments(
+				'vk_comments',
+				{pageUrl: window.location.href},
+				window.location.href
+			)
+		}
+	}
 	render() {
 		const {props} = this
 		const className = cls(props.className, "CommentsList")
 		return 	<Row className={className}>
 					<Col xs={12}>
 						<Paper zDepth={3}>
-							<VK apiId={Number(process.env.VK_ID)}>
-								<Comments />
-								{/* onNewComment={() => console.log('1')}  */}
-							</VK>
+							<div id="vk_comments" />
 						</Paper>
 					</Col>
 				</Row>
