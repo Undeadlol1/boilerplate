cd326991ba01ead4aed88321cf02b4e194b11c9c
undeadlol1 <undeadlol1@mail.ru>
2018-01-11 09:32:49 +0500

    add "isAdmin" permission checker

--- src/server/services/permissions.js
+++ src/server/services/permissions.js
@@ -1,4 +1,4 @@
-import Boom from 'boom';
+import Boom from 'boom'
 
 /**
  * TODO
@@ -8,4 +8,13 @@ import Boom from 'boom';
  */
 export function mustLogin(req, res, next) {
     req.isAuthenticated() ? next() : res.boom.unauthorized('Please, log in to do this')
+}
+
+/**
+ * check administrator permissions
+ */
+export function isAdmin(req, res, next) {
+    req.user.id === process.env.ADMIN_ID
+    ? next()
+    : res.boom.unauthorized('You must be an administrator to do this')
 }
