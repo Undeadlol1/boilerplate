33d579b059529ba5a5b4dcd9d10aecf878e5fc0f
undeadlol1 <undeadlol1@mail.ru>
2018-02-25 05:46:29 +0500

    (IndexPage) refactoring

--- src/browser/pages/IndexPage/IndexPage.jsx
+++ src/browser/pages/IndexPage/IndexPage.jsx
@@ -1,27 +1,19 @@
 // dependencies
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
-import React, { PureComponent } from 'react'
-import { Grid, Row } from 'react-styled-flexboxgrid'
+import React, { Component } from 'react'
 // project files
-import store from 'browser/redux/store'
-import Wysiwyg from 'browser/components/Wysiwyg'
 import { t } from 'browser/containers/Translator'
-import MoodsFind from 'browser/components/MoodsFind'
-import MoodsList from 'browser/components/MoodsList'
 import ForumsList from 'browser/components/ForumsList'
 import PageWrapper from 'browser/components/PageWrapper'
 import WelcomeCard from 'browser/components/WelcomeCard'
 import ThreadsList from 'browser/components/ThreadsList'
 import CreateForumForm from 'browser/components/CreateForumForm'
 
-class IndexPage extends PureComponent {
+class IndexPage extends Component {
     render() {
 		const { props } = this
-		return 	<PageWrapper
-					className='IndexPage'
-					loading={props.loading}
-				>
+		return 	<PageWrapper className='IndexPage'>
 					<WelcomeCard />
 					<b>{t('forums_list')}:</b>
 					<ForumsList />
@@ -30,22 +22,11 @@ class IndexPage extends PureComponent {
     }
 }
 
-IndexPage.propTypes = {
-	moods: PropTypes.object,
-	totalPages: PropTypes.number,
-	currentPage: PropTypes.number,
-	loading: PropTypes.bool.isRequired,
-	location: PropTypes.object.isRequired,
-}
+IndexPage.propTypes = {}
 
 export { IndexPage }
 
 export default
 connect(
-	({mood}) => ({
-		moods: mood.get('moods'),
-		loading: mood.get('loading'),
-		totalPages: mood.get('totalPages'),
-		currentPage: mood.get('currentPage'),
-	}),
+	(state, ownProps) => ({...ownProps})
 )(IndexPage)
