51c12cebdedcd718a15a88ca6d883fc865bbaf5c
Mihail Paley <undeadlol1@mail.ru>
2018-03-16 15:51:15 +0500

    merge "dockerize"

--- src/server/data/config.js
+++ src/server/data/config.js
@@ -1,30 +1,61 @@
 var config = process.env
+var isDocker = require('is-docker')()
 
 /*
-  Sometimes application uses this config outside of webpack
-    (migrations as an example).
+  Sometimes application uses this config outside of webpack (migrations as an example).
   Other times webpack compiles this file in code but we try to avoid requiring json files
-    manually because currently it causes endless recompiling
+  manually because currently it causes endless recompiling
   To avoid this, we need to require file manually only if it's neccesery
 */
 if (process.env.NODE_ENV == 'production' && config.dialect == undefined) config = require('../../../production.json')
+/*
+  This script will prepare database for dev/testing
+  by creating databases and user if they do not exist.
+  Usefull for docker containers and CI/CD pipelines.
+*/
+if (isDocker && (process.env.NODE_ENV == 'development' || 'test')) {
+  var mysql      = require('mysql');
+  var connection = mysql.createConnection({
+    host     : "db",
+    user     : "root",
+    password : null,
+    database : "boilerplate_test",
+    multipleStatements: true, // multistatements query
+  });
+  connection.query(
+    'CREATE DATABASE IF NOT EXISTS boilerplate_dev;'
+    + 'CREATE DATABASE IF NOT EXISTS boilerplate_test;'
+    + "CREATE USER IF NOT EXISTS 'root'@'db';"
+    + "GRANT ALL PRIVILEGES ON * . * TO 'root'@'db';"
+    + 'FLUSH PRIVILEGES;',
+    function (error, results, fields) {
+      if (error)  throw error
+    }
+  );
+}
 
 module.exports = {
   "development": {
-    "username": "root",
+    "port": "3306",
     "password": null,
-    "database": "boilerplat_dev",
-    "host": "127.0.0.1",
+    "logging": false,
     "dialect": "mysql",
-    "logging": false
+    "username": "root",
+    "database": "boilerplate_dev",
+    // In docker containers database will have special host.
+    // In non-container it will be connected through localhost.
+    "host": isDocker ? "db" : "127.0.0.1",
   },
   "test": {
-    "username": "root",
+    "port": "3306",
+    "logging": false,
     "password": null,
-    "database": "boilerplat_test",
-    "host": "127.0.0.1",
     "dialect": "mysql",
-    "logging": false
+    "username": "root",
+    "database": "boilerplate_test",
+    // In docker containers database will have special host.
+    // In non-container it will be connected through localhost.
+    "host": isDocker ? "db" : "127.0.0.1",
   },
   "production": {
     "username": config.DB_USER,
