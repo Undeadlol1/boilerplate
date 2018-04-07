7ec453bef5b004a81f28bd96655bdf4ce7f188a6
Mihail Paley <undeadlol1@mail.ru>
2018-04-07 21:26:37 +0500

    (bdd.test) rework

--- src/server/test/BDD/test.of.bdd.test.js
+++ src/server/test/BDD/test.of.bdd.test.js
@@ -1,20 +1,28 @@
 var Nightmare = require('nightmare');
 var expect = require('chai').expect; // jshint ignore:line
-var server = require('../../../server/server')
+var config = require('../../../../development.json')
+// var server = require('../../../server/server').default
 // TODO make this work.
 // 1) fire up the server on testing start
-
-describe('test duckduckgo search results', function() {
-    // before((done) => {
-    //     // console.log('server', server.default)
-    //     server.listen(3001, done)
+// let app = server.listen(process.env.PORT)
+describe('sample bdd test', function() {
+    // before(() => {
+    //     // Stop server incase it is running.
+    //     server.close()
+    //     // app && app.stop()
+    //     app = server.listen(process.env.PORT)
     // })
-    // before(() => server.stop())
-    it('should find the nightmare github link first', async function() {
-        await Nightmare()
-            .goto(process.env.URL)
-            .click('#MoodsList__item')
+    // after(() => {
+    //     server.close()
+    // })
+    it('Sample website crawler', async function() {
+        return await Nightmare()
+            .goto(config.URL)
+            .click('.NavBar')
             .end()
-            .then(result => console.log(result))
+            .then(result => {
+                console.log('result: ', result);
+            })
+            .catch(error => {throw error})
     })
 });
