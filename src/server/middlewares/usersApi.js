6c343a5c1af91c709cfaea4406b330aaa8e17b79
Mihail Paley <undeadlol1@mail.ru>
2018-04-12 20:36:53 +0500

    (UsersApi) refactoring

--- src/server/middlewares/usersApi.js
+++ src/server/middlewares/usersApi.js
@@ -1,50 +1,46 @@
 import { User, Local,  Profile, Vk, Twitter } from '../data/models'
 import { getUsersMoods } from '../data/controllers/MoodsController'
 import { mustLogin } from '../services/permissions'
-import { Router } from 'express'
+import asyncHandler from 'express-async-handler'
 import merge from 'lodash/assignIn'
+import { Router } from 'express'
 
 // routes
-const limit = 12
-
 export default Router()
-  .get('/user/:id', async function({ params }, res) {
-    try {
-      const id  = params.id
-      if (!id) return res.badRequest('invalid query')
+  /**
+   * Get user by id.
+   */
+  .get('/user/:id', asyncHandler(async (req, res) => {
+    const { id } = req.params
+    if (!id) return res.badRequest('invalid query')
 
-      const user =  await User.findById(id, {
-                      raw: true,
-                      nest: true,
-                      // TODO remove in future when all values will be stored in profile
-                      include: [Profile, Local, Vk, Twitter],
-                    })
-      const moodsData = await getUsersMoods(user.id)
+    const user = await User.findById(id, {
+      raw: true,
+      nest: true,
+      // TODO remove in future when all values will be stored in profile
+      include: [Profile, Local, Vk, Twitter],
+    })
+    const moodsData = await getUsersMoods(user.id)
 
-      if (!user) res.boom.notFound('user not found')
-      else res.json(merge(user, moodsData))
-
-    } catch (error) {
-      console.error(error)
-      res.boom.internal(error)
-    }
-  })
-
-  // currently used to update user.Profile.language
-  .put('/user/:id', mustLogin, async function({ params, body }, res) {
-    try {
-      // TODO add user validations/permissions
+    if (!user) res.boom.notFound('user not found')
+    else res.json(merge(user, moodsData))
+  }))
+  /**
+   * Update user.
+   * (currently used to update user.Profile.language)
+   */
+  // TODO add user validations/permissions
+  // NOTE: only owner  can owner can update his user profile.
+  .put('/user/:id', mustLogin, asyncHandler(async function({ params, body }, res) {
       const id  = params.id
 
       if (!id) return res.badRequest('invalid query')
       // TODO add body validations
-
+      // Check if user exists.
       const user = await User.findOne({
-                          raw: true,
-                          nest: true,
-                          where: {id},
-                          include: [Profile],
-                        })
+        where: {id},
+        include: [Profile],
+      })
       if (!user) return res.boom.notFound('user not found')
 
       // check if profile exists, create it if not
@@ -60,18 +56,10 @@ export default Router()
           {where: {UserId: user.id}}
         )
       }
-
-      const userWithProfile = await User.findOne({
-        where: {id},
-        include: [Profile],
-        raw: true,
-        nest: true,
-      })
-
-      res.json(userWithProfile)
-
-    } catch (error) {
-      console.error(error)
-      res.boom.internal(error)
-    }
-  })
+      // Respond with updated user document.
+      res.json(
+        await User.findOne({
+          where: { id },
+          include: [Profile],
+      }))
+  }))
--- src/server/test/middlewares/usersApi.test.js
+++ src/server/test/middlewares/usersApi.test.js
@@ -9,57 +9,39 @@ chai.should();
 
 export default describe('/users API', function() {
 
-    before(function() {
-        // TODO add logout? to test proper user login?
-        // Kill supertest server in watch mode to avoid errors
-        server.close()
-    })
-
-    after(function() {
-        server.close()
-    })
+    // Kill supertest server in watch mode to avoid errors
+    before(() => server.close())
+    after(() => server.close())
 
     it('get one user', async function() {
-        try {
-            const user = await User.findOne({order: sequelize.random()})
-            await request(server)
-                .get(`/api/users/user/${user.id}`)
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .then(function({body}) {
-                    assert(body.id, 'must have an id')
-                    assert(body.Profile, 'must have Profile')
-                    expect(body.moods).to.be.a('array')
-                    // assert(body.moods[0].UserId == body.id)
-                    // TODO
-                    // assert(!body.Local, 'must not have Local')
-                })
-        }
-        catch (error) {
-            console.error(error)
-            throw new Error(error)
-        }
+        const user = await User.findOne({order: sequelize.random()})
+        await request(server)
+            .get(`/api/users/user/${user.id}`)
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(function({body}) {
+                assert(body.id, 'must have an id')
+                assert(body.Profile, 'must have Profile')
+                expect(body.moods).to.be.a('array')
+                // assert(body.moods[0].UserId == body.id)
+                // TODO
+                // assert(!body.Local, 'must not have Local')
+            })
     })
 
     it('update user profile', async function() {
-        try {
-            const {username, password} = users[0]
-            const localAuth = await Local.findOne({where: {username}})
-            const agent = await loginUser(username, password)
-            await agent
-                .put(`/api/users/user/${localAuth.UserId}`)
-                .send({language: 'fr'})
-                .expect(200)
-                .expect('Content-Type', /json/)
-                .then(function(res) {
-                    const language = select('body.Profile.language', res)
-                    assert(language == 'fr', 'changed language improperly')
-                })
-        }
-        catch (error) {
-            console.error(error)
-            throw new Error(error)
-        }
+        const {username, password} = users[0]
+        const localAuth = await Local.findOne({where: {username}})
+        const agent = await loginUser(username, password)
+        await agent
+            .put(`/api/users/user/${localAuth.UserId}`)
+            .send({language: 'fr'})
+            .expect(200)
+            .expect('Content-Type', /json/)
+            .then(function(res) {
+                const language = select('body.Profile.language', res)
+                assert(language == 'fr', 'changed language improperly')
+            })
     })
 
     // it('must fail to update other user', async function() {
