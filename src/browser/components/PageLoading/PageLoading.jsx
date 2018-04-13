5dd883c96287e32212972f6a3636d7304ba3ae30
Mihail Paley <undeadlol1@mail.ru>
2018-04-13 16:30:59 +0500

    (PageLoading) add image indicator after SSR
    Signed-off-by: Mihail Paley <undeadlol1@mail.ru>

--- src/browser/components/PageLoading/PageLoading.jsx
+++ src/browser/components/PageLoading/PageLoading.jsx
@@ -6,14 +6,52 @@ import { Row, Col } from 'react-styled-flexboxgrid'
 import CircularProgress from 'material-ui/CircularProgress'
 import { translate as t } from 'browser/containers/Translator'
 
+const indicatorStyles = {
+	top: '50%',
+	left: '50%',
+	width: '100%',
+	height: '100%',
+	zIndex: '10000',
+	position: 'fixed',
+}
+
+const containerStyles = {
+	top: '0px',
+	left: '0px',
+	opacity: '1',
+	zIndex: '1200',
+	width: '100%',
+	height: '100%',
+	position: 'fixed',
+	willChange: 'opacity',
+	pointerEvents: 'auto',
+	transform: 'translateZ(0px)',
+	backgroundColor: 'rgba(0, 0, 0, 0.54)',
+    transition: 'left 0ms cubic-bezier(0.23, 1, 0.32, 1) 0ms, opacity 400ms cubic - bezier(0.23, 1, 0.32, 1) 0ms',
+}
+
+const imageStyles = {
+	transform: 'translate(-50%, -50%)',
+}
+/**
+ * This component displays full page loading indicator.
+ * NOTE: when user first sees the page after SSR React scripts will not be avalaible for a few moments.
+ * This means animated loading indicator will not work.
+ * So, instead we must show appliacation logo image as a loading indicator.
+ */
 class PageLoading extends Component {
 	render() {
 		const {props} = this
+		const isBrowser = process.env.BROWSER
 		const className = cls(props.className, "PageLoading")
 		if (!props.loading) return null
-		return 	<div className={className}>
-					<div className="PageLoading__indicator-container">
-						<CircularProgress className="PageLoading__indicator" />
+		return 	<div className={className} style={containerStyles}>
+					<div className="PageLoading__indicator-container" style={indicatorStyles}>
+						{
+							isBrowser
+							? <CircularProgress className="PageLoading__indicator" />
+							: <img src="/android-chrome-192x192.png" style={imageStyles} />
+						}
 					</div>
 				</div>
 	}
