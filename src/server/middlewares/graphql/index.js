e10676213bcf15aaac26cfde4279446b231aab99
Mihail Paley <undeadlol1@mail.ru>
2018-04-18 16:30:09 +0500

    (grapqhl) add basic mutation entry point
    Signed-off-by: Mihail Paley <undeadlol1@mail.ru>

--- src/server/middlewares/graphql/index.js
+++ src/server/middlewares/graphql/index.js
@@ -11,17 +11,22 @@ import forums from './types/forums'
 
 const schema = new GraphQLSchema({
     query: new GraphQLObjectType({
-        name: 'Root',
+        name: 'RootQuery',
         description: 'Every graphql api must start from a root query point.\n This is it.',
         fields: () => ({
             forum,
             forums,
         })
+    }),
+    mutation: new GraphQLObjectType({
+        name: 'RootMutation',
+        description: 'Every graphql api must start with a root mutation.',
+        // fields: mutations
+        fields: () => ({
+            forum,
+            forums,
+        })
     })
-    // mutation: new GraphQLObjectType({
-    //     name: 'Root mutation',
-    //     fields: mutations
-    // })
 })
 
 export default schema
