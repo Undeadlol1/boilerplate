2e4a1d418d6749e3f5727abb5e3dbb7abfbb5d43
Mihail Paley <undeadlol1@mail.ru>
2018-04-21 21:22:14 +0500

    (CreateForumForm) refatoring

--- src/browser/components/CreateForumForm/CreateForumForm.jsx
+++ src/browser/components/CreateForumForm/CreateForumForm.jsx
@@ -1,11 +1,11 @@
 import cls from 'classnames'
 import get from 'lodash/get'
-import gql from "graphql-tag"
+import gql from 'graphql-tag'
 import PropTypes from 'prop-types'
+import { compose } from 'recompose'
 import { connect } from 'react-redux'
 import store from 'browser/redux/store'
 import Dialog from 'material-ui/Dialog'
-import { stringify } from 'query-string'
 import React, { Component } from 'react'
 import FlatButton from 'material-ui/FlatButton'
 import { TextField } from 'redux-form-material-ui'
@@ -14,13 +14,58 @@ import RaisedButton from 'material-ui/RaisedButton'
 import { query as forumsQuery } from '../ForumsList'
 import { translate } from 'browser/containers/Translator'
 import { Grid, Row, Col } from 'react-styled-flexboxgrid'
-import ContentAdd from 'material-ui/svg-icons/content/add'
-import browserHistory from 'react-router/lib/browserHistory'
 import { actions } from 'browser/redux/actions/GlobalActions'
 import { parseJSON } from'browser/redux/actions/actionHelpers'
 import { insertForum } from 'browser/redux/forum/ForumActions'
-import { Mutation, MutationFunc, graphql } from "react-apollo"
-import FloatingActionButton from 'material-ui/FloatingActionButton'
+import withGraphQL from 'react-apollo-decorators/lib/withGraphQL'
+import withMutation from 'react-apollo-decorators/lib/withMutation'
+import { Mutation, MutationFunc, graphql, Query } from 'react-apollo'
+
+class CreateForumForm extends Component {
+
+	static propTypes = {
+		UserId: PropTypes.number,
+		reset: PropTypes.func.isRequired,
+		createForum: PropTypes.func.isRequired,
+	}
+
+	handleSubmit = variables => {
+		this.props
+		.createForum({ variables })
+		.then(() => this.props.reset())
+	}
+
+	render() {
+		console.log('this.props: ', this.props);
+		console.log('viewer: ', get(this, 'props.data.viewer.id') );
+		// Hide component if user is not admin.
+		if (get(this, 'props.viewer.id') != process.env.ADMIN_ID) return null
+		const { props } = this
+		const { handleSubmit, asyncValidating } = props
+		const classNames = cls(props.className, "CreateForumForm")
+		const isDisabled = asyncValidating == 'name' || props.submitting || props.loading || !props.valid
+		return <Row className={classNames}>
+			<Col xs={12}>
+				<form onSubmit={handleSubmit(this.handleSubmit)}>
+					<Field
+						fullWidth
+						name="name"
+						component={TextField}
+						hidden={asyncValidating}
+						hintText={translate("add_something")}
+					/>
+					<center>
+						<RaisedButton
+							type="submit"
+							primary={true}
+							disabled={isDisabled}
+							label={translate('submit')} />
+					</center>
+				</form>
+			</Col>
+		</Row>
+	}
+}
 
 const createForum = gql`
   mutation createForum($name: String!) {
@@ -41,127 +86,6 @@ const getCurrentUser = gql`
   }
 `
 
-@graphql(getCurrentUser)
-@reduxForm({
-	form: 'CreateForumForm',
-	validate(values) {
-		let errors = {}
-		const user = store.getState().user.get('id')
-
-		if (!user) errors.name = translate('please_login')
-		if (!values.name) errors.name = translate('name_cant_be_empty')
-		// if (!values.text) errors.text = translate('cant_be_empty')
-
-		return errors
-	},
-	onSubmit(values, dispatch, props) {
-		// 		function insertSucces(slug) {
-		// 			ownProps.reset()
-		// 			browserHistory.push('/mood/' + slug);
-		// 		}
-		//         // dispatch(insertForum(values, insertSucces))
-	}
-})
-export class CreateForumForm extends Component {
-
-	handleSubmit = variables => {
-		this.props
-		.createForum({ variables })
-		.then(() => this.props.reset())
-
-	}
-
-	render() {
-		// Hide component if user is not admin.
-		if (get(this, 'props.data.viewer.id') != process.env.ADMIN_ID) return null
-		const { props } = this
-		const { insertForum, handleSubmit, asyncValidating } = props
-		const classNames = cls(props.className, "CreateForumForm")
-		const isDisabled = asyncValidating == 'name' || props.submitting
-		console.log('data', props.data);
-		console.log('errors', props.errors)
-		console.log('error', props.error)
-		console.log('loading', props.loading);
-		return <Row className={classNames}>
-			<Col xs={12}>
-				<form onSubmit={handleSubmit(this.handleSubmit)}>
-					<Field
-						fullWidth
-						name="name"
-						component={TextField}
-						hidden={asyncValidating}
-						hintText={translate("add_something")}
-					/>
-					{/* <Field
-						rows={2}
-						fullWidth
-						name="text"
-						multiLine={true}
-						component={TextField}
-						hidden={asyncValidating}
-						hintText={translate("description")}
-					/> */}
-					<center>
-						<RaisedButton
-							type="submit"
-							primary={true}
-							disabled={!props.valid}
-							label={translate('submit')} />
-					</center>
-				</form>
-			</Col>
-		</Row>
-	}
-}
-
-CreateForumForm.propTypes = {
-	UserId: PropTypes.number,
-}
-
-// TODO reorganize this for better testing
-const form = reduxForm({
-	form: 'CreateForumForm',
-	validate(values) {
-		let errors = {}
-		const user = store.getState().user.get('id')
-
-		if (!user) errors.name = translate('please_login')
-		if (!values.name) errors.name = translate('name_cant_be_empty')
-		// if (!values.text) errors.text = translate('cant_be_empty')
-
-		return errors
-	},
-	onSubmit(values, dispatch, props) {
-	// 		function insertSucces(slug) {
-	// 			ownProps.reset()
-	// 			browserHistory.push('/mood/' + slug);
-	// 		}
-    //         // dispatch(insertForum(values, insertSucces))
-	}
-})
-// (CreateForumForm)
-// (connect(
-// 	(state, ownProps) => ({
-// 		...ownProps,
-// 		UserId: state.user.get('id'),
-// 	}),
-//     (dispatch, ownProps) => ({
-//         insertForum(values) {
-// 			console.log('ownProps: ', ownProps);
-// 			console.log('insertForum')
-// 			// values.parentId = ownProps.parentId
-
-// 			// function insertSucces(forum) {
-// 			// 	ownProps.reset()
-// 			// 	// browserHistory.push('/forum/' + forum.slug);
-// 			// }
-//             // // dispatch(toggleDialog())
-//             // dispatch(insertForum(values, insertSucces))
-// 		}
-//     })
-// )(CreateForumForm))
-// console.log('form: ', new form());
-
 // Update apollo cache to push
 // created forum into "forums" array.
 function update(cache, { data: { forum } }) {
@@ -172,8 +96,30 @@ function update(cache, { data: { forum } }) {
 	});
 }
 
-const mutation = props => <Mutation mutation={createForum} update={update}>
-	{(createForum, response) => (<CreateForumForm {...props} {...response} createForum={createForum}/>)}
-</Mutation>
+const formValidations = reduxForm({
+	form: 'CreateForumForm',
+	validate(values) {
+		let errors = {}
+		const user = store.getState().user.get('id')
 
-export default mutation
+		if (!user) errors.name = translate('please_login')
+		if (!values.name) errors.name = translate('name_cant_be_empty')
+		// if (!values.text) errors.text = translate('cant_be_empty')
+
+		return errors
+	}
+})
+
+const enhance = compose(
+	formValidations,
+	withGraphQL(getCurrentUser),
+	withMutation(createForum),
+)
+
+const mutation = props => (
+	<Mutation mutation={createForum} update={update}>
+		{(createForum, response) => (<CreateForumForm {...props} {...response} createForum={createForum} />)}
+	</Mutation>
+)
+
+export default enhance(mutation)
