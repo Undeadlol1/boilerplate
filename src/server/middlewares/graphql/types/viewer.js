9a29de50db388030a5b0dd00ba4344f74098f570
Mihail Paley <undeadlol1@mail.ru>
2018-04-28 01:08:28 +0500

    (graphql/viewer) clean up

--- src/server/middlewares/graphql/types/viewer.js
+++ src/server/middlewares/graphql/types/viewer.js
@@ -1,20 +1,4 @@
-import {
-    graphql,
-    GraphQLID,
-    GraphQLString,
-    GraphQLSchema,
-    GraphQLNonNull,
-    GraphQLUnionType,
-    GraphQLObjectType,
-} from 'graphql'
-import {
-    resolver,
-    attributeFields,
-    defaultArgs as attributesToArgs,
-} from 'graphql-sequelize'
 import { userType } from './user'
-import assign from 'lodash/assign'
-import { User } from '../../../data/models'
 /**
  * This is a full query interface for a logged in user.
  * @export
