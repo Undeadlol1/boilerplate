270fa2fa59769128b88f9308d7d1e446fd4cae1e
Mihail Paley <undeadlol1@mail.ru>
2018-04-29 18:54:03 +0500

    (LoginLogoutButton) rework with apollo

--- src/browser/components/LoginLogoutButton.jsx
+++ src/browser/components/LoginLogoutButton.jsx
@@ -1,29 +1,36 @@
+import get from 'lodash/get'
 import PropTypes from 'prop-types'
+import { compose } from 'recompose'
 import classNames from 'classnames'
 import extend from 'lodash/assignIn'
 import { connect } from 'react-redux'
 import React, { Component } from 'react'
+import { Mutation, Query } from 'react-apollo'
 import RaisedButton from 'material-ui/RaisedButton'
+import { getCurrentUser, logoutUser } from '../graphql'
 import { translate } from 'browser/containers/Translator'
-import { toggleLoginDialog, logoutCurrentUser } from 'browser/redux/actions/UserActions'
+import {
+	toggleLoginDialog,
+	logoutCurrentUser,
+} from 'browser/redux/actions/UserActions'
 /**
- * conditionally render 'login' or 'logout' button
+ * Conditionally render 'login' or 'logout' button.
  */
 export class LoginLogoutButton extends Component {
 	render() {
-		const { userId, className, logout, toggleDialog, inline, ...rest } = this.props
-		const label = translate(userId ? "logout" : "login")
+		const { className, logout, toggleDialog, inline, } = this.props
+		const UserId = get(this.props, 'data.viewer.id')
+		const label = translate(UserId ? "logout" : "login")
 		const inlineStyles = {display: 'block', textAlign: "center"}
 
 		const properties = extend(
 			{
-				onClick: userId ? logout : toggleDialog,
+				onClick: UserId ? logout : toggleDialog,
 				className: classNames('LoginLogoutButton', className),
 			},
 			// use <RaisedButton /> or <span> if 'inline' is specified
 			// span needs special styles and RaisedButton needs 'label'
 			inline ? {style: inlineStyles} : { label },
-			rest
 		)
 
 		if (inline) return <span {...properties}>{label}</span>
@@ -33,19 +40,67 @@ export class LoginLogoutButton extends Component {
 
 LoginLogoutButton.propTypes = {
 	inline: PropTypes.bool,
-	userId: PropTypes.number,
+	UserId: PropTypes.number,
 	logout: PropTypes.func.isRequired,
 	toggleDialog: PropTypes.func.isRequired,
 }
-
+/**
+ * Map redux state to component properties.
+ * NOTE: data fetching is moved to apollo,
+ * but this is still kep here just incase.
+ * TODO: remove in the future.
+ */
+export const stateToProps = ({ user }, ownProps) => ({
+	// UserId: user.get('id'),
+	...ownProps,
+})
+/**
+ * Pass down "toggleSidebar" action.
+ */
 export const dispatchToProps = (dispatch, ownProps) => ({
-	logout: () => dispatch(logoutCurrentUser()),
+	// logout: () => dispatch(logoutCurrentUser()),
 	toggleDialog: () => dispatch(toggleLoginDialog()),
 })
+/**
+ * Add graphlq mutation functionality to component.
+ * Mutation logs out current user and then updates apollo cache.
+ * @param {Object} ownProps
+ */
+const withMutation = ownProps => {
+	/**
+	 * Update apollo cache to remove current user object.
+	 * @param {Object} cache Apollo cache.
+	 * @param {Object} response Mutation response.
+	 */
+	function update(cache, response) {
+		return cache.writeQuery({
+			query: getCurrentUser,
+			data: { viewer: null }
+		})
+	}
+	return <Mutation mutation={logoutUser} update={update}>
+		{
+			(logout, props) => {
+				const properties = {
+					...props,
+					...ownProps,
+					logout,
+				}
+				return <LoginLogoutButton {...properties} />
+			}
+		}
+	</Mutation>
+}
+/**
+ * Fetch current user via apollo-graphql.
+ */
+const withQuery = props => (
+	<Query query={getCurrentUser}>
+		{({ data, error, loading }) => {
+			const properties = { data, error, loading, ...props }
+			return withMutation(properties)
+		}}
+	</Query>
+)
 
-export default connect(
-	({ user }, ownProps) => ({
-		userId: user.get('id'), ...ownProps
-	}),
-	dispatchToProps
-)(LoginLogoutButton)
+export default connect(stateToProps, dispatchToProps)(withQuery)
