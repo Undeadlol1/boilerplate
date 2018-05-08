2c3b625e1cc93a00fc5c5fd66de31e21b87c51cb
Mihail Paley <undeadlol1@mail.ru>
2018-05-09 02:38:54 +0500

    (mutations) add "createForum" tests

--- src/server/middlewares/graphql/mutations/index.js
+++ src/server/middlewares/graphql/mutations/index.js
@@ -8,54 +8,58 @@ import get from 'lodash/get'
 import logger from 'debug-logger'
 import assert from 'assert-plus'
 import extend from 'lodash/assign'
-import { logoutUser } from '../../authApi'
+import isEmpty from 'lodash/isEmpty'
+import { logoutUser as logout } from '../../authApi'
 import { userType } from '../queries/user'
 import { forumType } from '../queries/forum'
 import { Forums } from '../../../data/models'
 
 const debug = logger('mutations')
-
-export default {
-    /**
-     * Forum is a container for threads.
-     * Only admins can create forums.
-     */
-    createForum: {
-        type: forumType,
-        description: 'Create a forum.',
-        args: {
-            name: {
-                type: new GraphQLNonNull(GraphQLString)
-            },
-        },
-        resolve: async (source, args, { user }) => {
-            try {
-                // Debug logging.
-                debug('args', args)
-                debug('source', source)
-                debug('user && user.id', user && user.id)
-                // Verify data.
-                assert.object(user, "User")
-                assert.string(args.name)
-                // Prepare variables.
-                const   UserId = get(user, 'id'),
-                        slug = slugify(args.name),
-                        payload = extend(args, {UserId, slug})
-                // Verify permissions.
-                // TODO: add tests for both permissions.
-                // TODO: add tests for arguments.
-                if (!user) throw new Error('You must be logged in to do this.')
-                if (UserId !== process.env.ADMIN_ID) throw new Error('You must be an admin to do this.')
-                return await Forums.create(payload)
-            }
-            catch (err) {
-                throw err.message
-            }
+/**
+ * Mutation which reates forum.
+ * Forum is a container for threads.
+ * Only admins can create forums.
+ * @export
+ */
+export const createForum = {
+    type: forumType,
+    description: 'Create a forum.',
+    // Only "name" is ruquired to create forum.
+    args: {
+        name: {
+            type: new GraphQLNonNull(GraphQLString)
         },
     },
-    logoutUser: {
-        type: GraphQLBoolean, // Return nothing.
-        description: 'Logout current user.',
-        resolve: (source, args, req) => logoutUser(req),
-    }
+    resolve: async (source, args, {user}) => {
+        try {
+            // Debug logging.
+            debug('args', args)
+            debug('source', source)
+            debug('user && user.id', user && user.id)
+            // Prepare variables.
+            const UserId = get(user, 'id'),
+                slug = slugify(args.name),
+                payload = extend(args, {
+                    UserId,
+                    slug
+                })
+            // Verify permissions.
+            // TODO: add tests for both permissions.
+            // TODO: add tests for arguments.
+            if (isEmpty(user)) throw new Error('You must be logged in to do this.')
+            if (UserId !== process.env.ADMIN_ID) throw new Error('You must be an admin to do this.')
+            return await Forums.create(payload)
+        } catch (err) {
+            throw err.message
+        }
+    },
+}
+/**
+ * Logout current user mutation.
+ * @export
+ */
+export const logoutUser = {
+    type: GraphQLBoolean, // Return nothing.
+    description: 'Logout current user.',
+    resolve: (source, args, req) => logout(req),
 }
--- src/server/middlewares/graphql/mutations/mutations.test.js
+++ src/server/middlewares/graphql/mutations/mutations.test.js
@@ -1,3 +1,4 @@
+import UUID from 'uuid/v4'
 import get from 'lodash/get'
 import { expect } from 'chai'
 import schema from '../index'
@@ -8,21 +9,92 @@ import { tester } from 'graphql-tester'
 import { User, Forums } from '../../../data/models'
 import { create } from 'graphql-tester/lib/main/servers/express'
 chai.use(require('chai-properties'))
-
-
+/**
+ * Define UserId to create user object for tests
+ * and to delete it after tests are done.
+ */
+const UserId = UUID()
 const payload = { name: 'This is a test' }
+const crerateForumQuery = `mutation {
+    forum: createForum(name: "${payload.name}") {
+        id
+        name
+        slug
+        UserId
+    }
+}`
 
-describe('Express Server', () => {
-
+describe('Mutations:', () => {
+    // TODO: do i need this?
     after(async () => {
-        await User.destroy({where: {id: "1"}})
+        await User.destroy({where: {id: UserId}})
     })
-
-    // const ExpressTest = tester({
-    //     url: '/graphql',
-    //     server: create(server),
-    // });
-
+    /**
+     * Verify that "cretaeForum" works properly.
+     */
+    describe('createForum returns', () => {
+        // Prevent unlogged users to create forums.
+        it('error if user is not logged in.', async () => {
+            // Run query and get results.
+            const { data, errors } = await graphql(
+                schema,
+                crerateForumQuery,
+                null,
+                // Notice empty user object in context.
+                {user: {}}
+            )
+            expect(data.forum).to.be.null
+            expect(errors).to.have.length(1)
+            expect(errors[0].message).to.equal('You must be logged in to do this.')
+        })
+        // Only administrators can create forums.
+        it('error if user is not an admin.', async () => {
+            // Get non-admin user.
+            const user = await User.findOne({})
+            // Run query and get results.
+            const { data, errors } = await graphql(
+                schema,
+                crerateForumQuery,
+                null,
+                // Ordinary user passed to context.
+                {user}
+            )
+            expect(data.forum).to.be.null
+            expect(errors).to.have.length(1)
+            expect(errors[0].message).to.equal('You must be an admin to do this.')
+        })
+        // Verify it returns created forum if everything is all right.
+        it('created document.', async () => {
+            // Create admin user.
+            const admin = await User.create({id: 1})
+            // Run query and get results.
+            const { data, errors } = await graphql(
+                schema,
+                crerateForumQuery,
+                null,
+                // Pass administrator as user in context.
+                {user: admin}
+            )
+            // Verify results.
+            expect(errors).to.be.undefined
+            // Make sure forum was inserted into database.
+            const createdForum = await Forums.findOne({where: payload})
+            expect(createdForum).to.be.a('object', 'forum should be created')
+            expect(createdForum).to.have.properties(data.forum)
+            // Make sure response has proper data.
+            expect(data.forum).to.have.properties({
+                UserId: admin.id,
+                name: payload.name,
+            })
+            // Delete created forum.
+            await createdForum.destroy()
+        })
+        // TODO: what if forum with this name already exists?
+    })
+    /**
+     * This tests were created when i was learning graphql.
+     * WIP. They will be moved to appropriate files.
+     */
     describe('Valid Query', () => {
 
         it('Returns success', async () => {
@@ -49,22 +121,22 @@ describe('Express Server', () => {
             const { data } = await graphql(schema, query)
             expect(data.forum).to.have.property("id", forum.id);
         })
-        it('example of a mutation', async () => {
-            const user  = await User.create({id: 1})
-            const query = `mutation {
-                createForum(name: "${payload.name}") {
-                    id
-                    name
-                    slug
-                    UserId
-                }
-            }`
-            const { data, errors } = await graphql(schema, query, null, {user})
-            const createdForum  = await Forums.findOne({where: payload})
-            assert.isUndefined(errors)
-            expect(createdForum).to.be.a('object', 'forum should be created')
-            expect(createdForum).to.have.properties(data.forum)
-        })
+        // it('example of a mutation', async () => {
+        //     const user  = await User.create({id: 1})
+        //     const query = `mutation {
+        //         createForum(name: "${payload.name}") {
+        //             id
+        //             name
+        //             slug
+        //             UserId
+        //         }
+        //     }`
+        //     const { data, errors } = await graphql(schema, query, null, {user})
+        //     const createdForum  = await Forums.findOne({where: payload})
+        //     assert.isUndefined(errors)
+        //     expect(createdForum).to.be.a('object', 'forum should be created')
+        //     expect(createdForum).to.have.properties(data.forum)
+        // })
         // it('Returns the correct Status code', () => {
         //     return response.should.eventually.have.property('status').equal(200);
         // });
