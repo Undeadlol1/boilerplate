713ddfaa59e17e7f66fa55e010f8feb18d33df5c
Mihail Paley <undeadlol1@mail.ru>
2018-05-11 07:20:01 +0500

    (CreateThreadForm) refactoring

--- src/browser/components/CreateThreadForm/CreateThreadForm.jsx
+++ src/browser/components/CreateThreadForm/CreateThreadForm.jsx
@@ -1,30 +1,23 @@
 import cls from 'classnames'
 import PropTypes from 'prop-types'
+import extend from 'lodash/assignIn'
 import Paper from 'material-ui/Paper'
 import { connect } from 'react-redux'
 import store from 'browser/redux/store'
-import Dialog from 'material-ui/Dialog'
-import { stringify } from 'query-string'
 import React, { Component } from 'react'
-import FlatButton from 'material-ui/FlatButton'
+import { Field, reduxForm } from 'redux-form'
 import { TextField } from 'redux-form-material-ui'
-import { Form, Field, reduxForm } from 'redux-form'
 import RaisedButton from 'material-ui/RaisedButton'
+import { Row, Col } from 'react-styled-flexboxgrid'
 import { translate } from 'browser/containers/Translator'
-import { Grid, Row, Col } from 'react-styled-flexboxgrid'
-import ContentAdd from 'material-ui/svg-icons/content/add'
 import browserHistory from 'react-router/lib/browserHistory'
-import { actions } from 'browser/redux/actions/GlobalActions'
-import { parseJSON } from'browser/redux/actions/actionHelpers'
 import { insertThread } from 'browser/redux/forum/ForumActions'
-import FloatingActionButton from 'material-ui/FloatingActionButton'
 
 export class CreateThreadForm extends Component {
 	render() {
 		const { props } = this
-		const { valid, insertThread, handleSubmit, dialogIsOpen, isValid, asyncValidating, className } = props
-		const classNames = cls(className, "CreateThreadForm")
-		const isDisabled = props.asyncValidating == 'name' || props.submitting
+		const classNames = cls(props.className, "CreateThreadForm")
+		const { insertThread, handleSubmit, asyncValidating } = props
 	    return 	<Row className={classNames}>
 					<Col xs={12}>
 						<Paper zDepth={5} className="CreateThreadForm__paper">
@@ -34,7 +27,7 @@ export class CreateThreadForm extends Component {
 									name="name"
 									component={TextField}
 									hidden={asyncValidating}
-									hintText={props.title || translate("create_thread")}
+									floatingLabelText={props.title || translate('create_thread')}
 								/>
 								<Field
 									rows={2}
@@ -43,14 +36,14 @@ export class CreateThreadForm extends Component {
 									multiLine={true}
 									component={TextField}
 									hidden={asyncValidating}
-									hintText={translate("description")}
+									floatingLabelText={translate("description")}
 								/>
 								<center>
 									<RaisedButton
 										type="submit"
 										primary={true}
 										label={translate('submit')}
-										disabled={!valid} />
+										disabled={!props.valid || props.submitting} />
 								</center>
 							</form>
 						</Paper>
@@ -66,7 +59,7 @@ CreateThreadForm.propTypes = {
 	parentId: PropTypes.string.isRequired,
 }
 
-// TODO reorganize this for better testing
+// Connect to redux-form to validate form input.
 export default reduxForm({
 	form: 'CreateThreadForm',
 	validate(values) {
@@ -79,30 +72,21 @@ export default reduxForm({
 
 		return errors
 	},
-	// onSubmit(values, dispatch, props) {
-	// 		function insertSucces(slug) {
-	// 			ownProps.reset()
-	// 			browserHistory.push('/mood/' + slug);
-	// 		}
-    //         // dispatch(insertThread(values, insertSucces))
-	// }
 })
+// Connect to Redux.
 (connect(
-	(state, ownProps) => ({
-		...ownProps,
-		dialogIsOpen: state.mood.get('dialogIsOpen'),
-	}),
-    (dispatch, ownProps) => ({
+	(state, ownProps) => ({...ownProps}),
+    (dispatch, {parentId, reset}) => ({
         insertThread(values) {
-			console.log('insertThread')
-			values.parentId = ownProps.parentId
-
+			// Provide "parentId" to payload data.
+			const payload = extend(values, {parentId})
+			// Reset form and change url on succes.
 			function insertSucces(thread) {
-				ownProps.reset()
+				reset()
 				browserHistory.push('/threads/' + thread.slug);
 			}
-            // dispatch(toggleDialog())
-            dispatch(insertThread(values, insertSucces))
+			// Call function.
+            dispatch(insertThread(payload, insertSucces))
 		}
     })
 )(CreateThreadForm))
