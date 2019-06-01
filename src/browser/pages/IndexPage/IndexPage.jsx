bba3083d1d6af942e6a1517a80ec40459ae2aa31
Mihail Paley <undeadlol1@mail.ru>
2019-06-01 12:16:11 +0500

    (IndexPage) remove unused dependencies

--- src/browser/pages/IndexPage/IndexPage.jsx
+++ src/browser/pages/IndexPage/IndexPage.jsx
@@ -1,36 +1,28 @@
 // dependencies
-import logger from 'debug-logger'
-import PropTypes from 'prop-types'
-import { connect } from 'react-redux'
-import React, { Component } from 'react'
+import { connect } from "react-redux";
+import React, { Component } from "react";
 // project files
-import { t } from 'browser/containers/Translator'
-import ForumsList from 'browser/components/ForumsList'
-import PageWrapper from 'browser/components/PageWrapper'
-import WelcomeCard from 'browser/components/WelcomeCard'
-import CreateForumForm from 'browser/components/CreateForumForm'
-
-const debug = logger('IndexPage')
+import { t } from "browser/containers/Translator";
+import ForumsList from "browser/components/ForumsList";
+import PageWrapper from "browser/components/PageWrapper";
+import WelcomeCard from "browser/components/WelcomeCard";
+import CreateForumForm from "browser/components/CreateForumForm";
 
 class IndexPage extends Component {
-    render() {
-		const { props } = this
-		return 	(
-			<PageWrapper className='IndexPage'>
-				<WelcomeCard />
-				<CreateForumForm />
-				<b>{t('forums_list')}:</b>
-				<ForumsList />
-			</PageWrapper>
-		)
-    }
+  render() {
+    return (
+      <PageWrapper className="IndexPage">
+        <WelcomeCard />
+        <CreateForumForm />
+        <b>{t("forums_list")}:</b>
+        <ForumsList />
+      </PageWrapper>
+    );
+  }
 }
 
-IndexPage.propTypes = {}
+IndexPage.propTypes = {};
 
-export { IndexPage }
+export { IndexPage };
 
-export default
-connect(
-	(state, ownProps) => ({...ownProps})
-)(IndexPage)
+export default connect((state, ownProps) => ({ ...ownProps }))(IndexPage);
