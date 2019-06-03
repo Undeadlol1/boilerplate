789ba8b58273c630ca266126c626cb90ea88180b
Mihail Paley <undeadlol1@mail.ru>
2019-06-03 18:28:21 +0500

    (graphq/user) refactoring

--- src/server/middlewares/graphql/queries/user.js
+++ src/server/middlewares/graphql/queries/user.js
@@ -1,19 +1,7 @@
-import {
-    graphql,
-    GraphQLID,
-    GraphQLString,
-    GraphQLSchema,
-    GraphQLNonNull,
-    GraphQLUnionType,
-    GraphQLObjectType,
-} from 'graphql'
-import {
-    resolver,
-    defaultArgs as attributesToArgs,
-    attributeFields,
-} from 'graphql-sequelize'
-import assign from 'lodash/assign'
-import { User } from '../../../data/models'
+import { GraphQLObjectType } from 'graphql';
+import { attributeFields, defaultArgs as attributesToArgs, resolver } from 'graphql-sequelize';
+import assign from 'lodash/assign';
+import { User } from '../../../data/models';
 
 const description = 'Registered website user.'
 
@@ -21,13 +9,13 @@ const userType = new GraphQLObjectType({
     description,
     name: 'user',
     // Get fields from sequelize model.
-    fields: assign(attributeFields(User, { commentToDescription: true }))
+    fields: assign(attributeFields(User, { commentToDescription: true })),
 })
 /**
  * Graphql type representing a User object.
  * @export
  */
-export { userType }
+export { userType };
 /**
  * This is a full query interface for a User.
  * @export
