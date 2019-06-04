9d51fff83fb8f7afc25d5c81828bc7049723934e
Mihail Paley <undeadlol1@mail.ru>
2019-06-04 16:36:07 +0500

    Revert "(tsconfig) "allowSyntheticDefaultImports": true"
    This reverts commit 34947070014e3811f1598c91f8db8eb1b76a9770.

--- src/browser/components/WelcomeCard/WelcomeCard.jsx
+++ src/browser/components/WelcomeCard/WelcomeCard.jsx
@@ -1,54 +1,53 @@
-import cls from "classnames";
-import cookies from "cookies-js";
-import PropTypes from "prop-types";
-import Paper from "material-ui/Paper";
-import React, { Component } from "react";
-import { withCookies } from "react-cookie";
-import FlatButton from "material-ui/FlatButton";
-import { Row, Col } from "react-styled-flexboxgrid";
-import { translate as t } from "browser/containers/Translator";
-import { Card, CardText, CardActions } from "material-ui/Card";
+import cls from 'classnames'
+import cookies from 'cookies-js'
+import PropTypes from 'prop-types'
+import Paper from 'material-ui/Paper'
+import React, { Component } from 'react'
+import { withCookies } from 'react-cookie'
+import FlatButton from 'material-ui/FlatButton'
+import { Row, Col } from 'react-styled-flexboxgrid'
+import { translate as t } from 'browser/containers/Translator'
+import { Card, CardText, CardActions } from 'material-ui/Card'
 
-class WelcomeCard extends Component<{}, {}> {
-  state = { isHidden: false };
+class WelcomeCard extends Component {
 
-  handleClick = () => {
-    cookies.set("hideWelcomeCard", true);
-    this.setState({ isHidden: true });
-  };
+	state = { isHidden: false }
 
-  render() {
-    const { state, props } = this;
-    const className = cls(props.className, "WelcomeCard");
-    if (state.isHidden || props.cookies.get("hideWelcomeCard")) return null;
-    return (
-      <Row className={className}>
-        <Col xs={12}>
-          <Paper zDepth={5}>
-            <Card>
-              <CardText>
-                <p>{t("welcome_to_our_awesome_boilerplate")}</p>
-                <p>{t("hope_you_will_enjoy_it")}</p>
-              </CardText>
-              <CardActions>
-                <FlatButton
-                  fullWidth
-                  label={t("ok")}
-                  primary={true}
-                  onClick={this.handleClick}
-                />
-              </CardActions>
-            </Card>
-          </Paper>
-        </Col>
-      </Row>
-    );
-  }
+	handleClick = () => {
+		cookies.set('hideWelcomeCard', true)
+		this.setState({ isHidden: true })
+	}
+
+	render() {
+		const {state, props} = this
+		const className = cls(props.className, "WelcomeCard")
+		if (state.isHidden || props.cookies.get('hideWelcomeCard')) return null
+		return 	<Row className={className}>
+					<Col xs={12}>
+						<Paper zDepth={5}>
+							<Card>
+								<CardText>
+									<p>{t('welcome_to_our_awesome_boilerplate')}</p>
+									<p>{t('hope_you_will_enjoy_it')}</p>
+								</CardText>
+								<CardActions>
+									<FlatButton
+										fullWidth
+										label={t('ok')}
+										primary={true}
+										onClick={this.handleClick}
+									/>
+								</CardActions>
+							</Card>
+						</Paper>
+					</Col>
+				</Row>
+	}
 }
 
-WelcomeCard.defaultProps = {};
+WelcomeCard.defaultProps = {}
 
-WelcomeCard.PropTypes = {};
+WelcomeCard.PropTypes = {}
 
-export { WelcomeCard };
-export default withCookies(WelcomeCard);
+export { WelcomeCard }
+export default withCookies(WelcomeCard)
