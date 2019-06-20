6464a7cd031556ddc94d5559697b550b85f50b1e
Mihail Paley <undeadlol1@mail.ru>
2019-06-20 20:42:44 +0500

    remove a TODO

--- src/server/data/models/user.js
+++ src/server/data/models/user.js
@@ -1,27 +1,26 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
+module.exports = function (sequelize, DataTypes) {
   var User = sequelize.define('User', {
-      role: {
-        allowNull: false,
-        defaultValue: 'user',
-        type: DataTypes.STRING,
-        comment: 'User can be an "admin" or a "user"',
-      },
-      // TODO comment
-      image: {
-        type: DataTypes.STRING,
-        comment: 'Link to user\'s avatar.'
-      },
-      /*
-        NOTE: with non-latin characters the only way to avoid "charset" erros using sequelize
-        was to change mysql config to accept utf-8 and changing table charsets manually via
-        ALTER TABLE db_name.table_name CONVERT TO CHARACTER SET utf8;
-      */
-      displayName: {
-        type: DataTypes.STRING,
-        comment: 'Human readable name of the user. Not unique. Updatable by user.'
-      },
+    role: {
+      allowNull: false,
+      defaultValue: 'user',
+      type: DataTypes.STRING,
+      comment: 'User can be an "admin" or a "user"',
     },
+    image: {
+      type: DataTypes.STRING,
+      comment: 'Link to user\'s avatar.',
+    },
+    /*
+      NOTE: with non-latin characters the only way to avoid "charset" erros using sequelize
+      was to change mysql config to accept utf-8 and changing table charsets manually via
+      ALTER TABLE db_name.table_name CONVERT TO CHARACTER SET utf8;
+    */
+    displayName: {
+      type: DataTypes.STRING,
+      comment: 'Human readable name of the user. Not unique. Updatable by user.',
+    },
+  },
     {
       tableName: 'users',
       freezeTableName: true,
